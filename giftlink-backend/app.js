a56e9f8ae1fe45164ce580eea216910713988f6c
Andres Barrios Arenas <andres@generaweb.net>
2025-07-18 17:24:29 -0600

    Develop Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,12 +24,11 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
-app.use('/api/gifts', giftRoutes);
+
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
-app.use('/api/search', searchRoutes);
-
+const authRoutes = require('./routes/authRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -38,11 +37,11 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
+app.use('/api/gifts', giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
